ae825121a285897e1beb667a250405d42ae7b3d3
BL-JyothsnaThota <jyothsna.thota@bridgelabz.com>
2025-12-29 17:10:27 +0530

    Random die value generating

--- src/game/Snake_Ladder.java
+++ src/game/Snake_Ladder.java
@@ -1,16 +1,21 @@
 package game;
-
+import java.util.Random;
 public class Snake_Ladder {
 
 	static final int NO_PLAY = 0;
     static final int LADDER = 1;
     static final int SNAKE = 2;
     static final int WIN_POSITION = 100;
+
+    static Random random = new Random();
+
     
-    
+    static int rollDie() {
+        return random.nextInt(6) + 1;
+    }
 	
 	public static void main(String[] args) {
-
+		
 	}
 	
 }
