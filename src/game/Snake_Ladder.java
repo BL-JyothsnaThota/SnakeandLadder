4c4aac7d789841373ec0e7760978b392a6541e80
BL-JyothsnaThota <jyothsna.thota@bridgelabz.com>
2025-12-29 15:41:16 +0530

    Initial position is 0

---
+++ src/game/Snake_Ladder.java
@@ -0,0 +1,16 @@
+package game;
+
+public class Snake_Ladder {
+
+	static final int NO_PLAY = 0;
+    static final int LADDER = 1;
+    static final int SNAKE = 2;
+    static final int WIN_POSITION = 100;
+    
+    
+	
+	public static void main(String[] args) {
+
+	}
+	
+}
